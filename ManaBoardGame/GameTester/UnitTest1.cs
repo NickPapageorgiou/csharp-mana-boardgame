e32ac6c2b8b89230561a984baa3c75575e420037
Nick <nickpapageo22@gmail.com>
2016-09-21 20:34:17 +0300

    First Stages of board game, including tester.

---
+++ ManaBoardGame/GameTester/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ManaBoardGame;
+
+namespace GameTester
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestInitialization()
+        {
+            Game game = new Game();
+            game.Initialize();
+
+        }
+
+        [TestMethod]
+        public void TestPlayerInteraction()
+        {
+            Game game = new Game();
+            int currentPlayer = game.GetCurrentPlayer();
+            int expectedPlayer = 1;
+            Assert.AreEqual(currentPlayer, expectedPlayer);
+
+ 
+
+        }
+
+        [TestMethod]
+        public void TestGameplay()
+        {
+            Game game = new Game();
+            int currentPlayer = game.GetCurrentPlayer();
+            int startX = 0, startY = 0, endX = 0, endY = 0;
+
+            int expectedPlayer = 1;
+            Assert.AreEqual(currentPlayer, expectedPlayer);
+            try
+            {
+                game.MoveTo(currentPlayer, startX, startY, endX, endY);
+
+            }
+            catch(IllegalMoveException e)
+            {
+                Assert.Fail(e.Message);
+            }
+            
+        }
+    }
+}
---
+++ ManaBoardGame/ManaBoardGame/Game.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ManaBoardGame
+{
+    public class Game
+    {
+        int[,] board;
+        int currentPlayer = 1;
+        public Game()
+        {
+            Initialize();
+
+        }
+
+        public void Initialize()
+        {
+            board = new int[,] {
+                {2,2,3,1,2,2},
+                {1,3,1,3,1,3},
+                {3,1,2,2,3,1},
+                {2,3,1,3,1,2},
+                {2,1,3,1,3,2},
+                {1,3,2,2,1,3} };
+
+            currentPlayer = 1;
+        }
+
+        public int GetCurrentPlayer()
+        {
+            return currentPlayer;
+        }
+        /// <exception cref="IllegalMoveException">Performing an illegal action on the board.</exception>
+        /// <exception cref="ArgumentException">Player must be 1 or 2. Move coords must be 0-5.</exception>
+        public void MoveTo(int player, int startX, int startY, int endX, int endY) 
+        {
+
+            throw new IllegalMoveException();
+            throw new ArgumentException();
+        }
+    }
+}
